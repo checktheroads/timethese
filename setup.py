8fcd686f32614a8870b47718106cb829f8fda848
checktheroads <mtt.lov@gmail.com>
2019-06-28 09:29:57 -0700

    Fix structure

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     name="timethese",
     version="0.0.1",
     python_requires=">=3.6",
+    packages=["timethese"],
     install_requires=[],
     license="Do What The F*ck You Want To Public License",
     long_description=open("README.md").read(),
