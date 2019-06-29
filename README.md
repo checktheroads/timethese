4fa8547d5716d68a6a103ec173bf601c001ae378
checktheroads <mtt.lov@gmail.com>
2019-06-28 17:48:46 -0700

    all the things

--- README.md
+++ README.md
@@ -15,9 +15,45 @@ time_tuple = "t = (0,1,2,3,4,5,6,7,8,9); len(t)"
 time_set = "s = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; len(s)"
 time_dict = 'd = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10}; len(d)'
 
-tuple_set_dict = timethese.Strings(time_tuple, time_set, time_dict)
+tuple_set_dict = timethese.Strings(actions=(time_tuple, time_set, time_dict))
 print(tuple_set_dict)
-# (62.068, 260.607, 332.321)
+# (59.917, 286.146, 330.834)
+```
+
+You can optionally pass setup parameters with `params="a = 1"`, which will be run *per* action.
+
+```python
+import timethese
+
+time_tuple = "len(t)"
+time_set = "len(s)"
+time_dict = "len(d)"
+
+param_tuple = "t = (0,1,2,3,4,5,6,7,8,9)"
+param_set = "s = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}"
+param_dict = 'd = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10}'
+
+tuple_set_dict = timethese.Strings(actions=(time_tuple, time_set, time_dict), params=(param_tuple, param_set, param_dict))
+print(tuple_set_dict)
+# (50.301, 53.56, 50.926)
+```
+
+Lastly, you can optionally pass global parameters with `globalparams={"a": 1}`, which will be added to Python's `globals()` built in function for all actions passed:
+
+```python
+import timethese
+
+time_tuple = "t + (to_ts,)"
+time_set = "s.add(to_ts)"
+time_dict = "d.update(to_d)"
+
+param_tuple = "t = (0,1,2,3,4,5,6,7,8,9)"
+param_set = "s = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}"
+param_dict = 'd = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10}'
+
+tuple_set_dict = timethese.Strings(actions=(time_tuple, time_set, time_dict), params=(param_tuple, param_set, param_dict), globalparams={"to_ts": 100, "to_d": {"eleven": 11}})
+print(tuple_set_dict)
+# (78.568, 76.308, 150.234)
 ```
 
 ## License
--- timethese/timethese.py
+++ timethese/timethese.py
@@ -6,20 +6,90 @@ Times the runtime of input actions, returns tuple of values in microseconds.
 import timeit
 
 
-def Strings(*actions, setup=None):
-    """Accept any number of strings for actions to execute, time each execution, convert each \
-    execution time to microseconds, append "usec" to each item, return tuple of all input run \
-    times in microseconds."""
-    for action in actions:
-        if not isinstance(action, str):
-            raise TypeError("Action must be a string")
-    if setup:
-        timer = lambda action: timeit.timeit(
-            action, setup, globals=globals(), number=1000000
-        )
-    if not setup:
-        timer = lambda action: timeit.timeit(action, globals=globals(), number=1000000)
-    msecs = tuple(timer(action) for action in actions)
-    usec = lambda msec: round(msec * 1000, 3)
-    usecs = tuple(usec(msec) for msec in msecs)
+def Strings(actions=(None,), params=(None,), globalparams={None: None}):
+    """
+    Accept any number of strings for actions to execute, time each execution, convert
+    each execution time to microseconds, append "usec" to each item, return tuple of all
+    input run times in microseconds.
+    """
+    # Actions Validation
+    # Handle no actions passed
+    if not all(actions):
+        raise AttributeError("No actions are specified")
+    # Handle tuple with non-strings
+    elif isinstance(actions, tuple) and not all(
+        tuple(map(lambda a: isinstance(a, str), actions))
+    ):
+        raise TypeError("Action items must be strings")
+    # Handle empty tuple
+    elif isinstance(actions, tuple) and False in actions:
+        raise TypeError("Action items must be strings")
+    # Handle standalone string not in tuple
+    elif not isinstance(actions, tuple) and isinstance(actions, str):
+        actions = (actions,)
+    # Handle non-tuples
+    elif not isinstance(actions, tuple):
+        raise TypeError("Actions must be tuples")
+    #
+    # Global Parameters Validation
+    # Handle non-dict
+    if not isinstance(globalparams, dict):
+        raise TypeError("Global Parameters must be a dictionary")
+    elif all(globalparams) and not all(
+        tuple(map(lambda gp: isinstance(gp, str), globalparams.keys()))
+    ):
+        raise TypeError("Global Parameter items must be strings")
+    usecs = None
+    if all(params):
+        #
+        # Instance Parameter Validation
+        # Handle standalone string not in tuple
+        if isinstance(params, str):
+            params = (params,)
+        # Handle non-tuples
+        elif not isinstance(params, tuple):
+            raise TypeError("Parameters must be tuples")
+        # Handle tuple with non-strings
+        elif (
+            isinstance(params, tuple)
+            and all(globalparams)
+            and not all(tuple(map(lambda a: isinstance(a, str), params)))
+        ):
+            raise TypeError("Parameter items must be strings")
+        # Handle empty tuple
+        elif isinstance(params, tuple) and len(params) == 0:
+            raise TypeError("Parameters cannot be empty if passed")
+        if all(globalparams):
+            timer = lambda action, param: timeit.timeit(
+                action, setup=param, globals=globalparams, number=1000000
+            )
+        elif not all(globalparams):
+            timer = lambda action, param: timeit.timeit(
+                action, setup=param, number=1000000
+            )
+        action_groups = tuple(zip(actions, params))
+        usecs = ()
+        for action_group in action_groups:
+            usecs += (
+                round(
+                    timer(
+                        [action for action in action_group][0],
+                        [action for action in action_group][1],
+                    )
+                    * 1000,
+                    3,
+                ),
+            )
+    elif not all(params):
+        if all(globalparams):
+            timer = lambda action: timeit.timeit(
+                action, globals=globalparams, number=1000000
+            )
+        elif not all(globalparams):
+            timer = lambda action: timeit.timeit(action, number=1000000)
+        usecs = ()
+        for action in actions:
+            usecs += (round(timer(action) * 1000, 3),)
+    else:
+        raise AttributeError("Error reading Instance Parameters")
     return usecs
